050f56726873ca4b3e410643c1ee79b07675e724
Joss Gray <joss@jossgray.net>
2014-08-27 21:28:29 +0100

    Add src

---
+++ zyrecffi/_cffi.py
@@ -0,0 +1,117 @@
+from cffi import FFI
+import os
+ffi = FFI()
+
+ffi.cdef('''
+typedef struct _zmsg_t zmsg_t;
+typedef struct _zsock_t zsock_t;
+typedef struct _zyre_t zyre_t;
+
+int zmsg_addstr (zmsg_t* self, const char* string);
+
+char* zmsg_popstr (zmsg_t* self);
+
+zyre_t* zyre_new (const char *name);
+
+void zyre_destroy (zyre_t **self_p);
+
+const char* zyre_uuid (zyre_t *self);
+
+const char *zyre_name (zyre_t *self);
+
+void zyre_set_header (zyre_t *self, const char *name, const char *format, ...);
+
+void zyre_set_verbose (zyre_t *self);
+
+void zyre_set_port (zyre_t *self, int port_nbr);
+
+void zyre_set_interval (zyre_t *self, size_t interval);
+
+void zyre_set_interface (zyre_t *self, const char *value);
+
+int zyre_set_endpoint (zyre_t *self, const char *format, ...);
+
+void zyre_gossip_bind (zyre_t *self, const char *format, ...);
+
+void zyre_gossip_connect (zyre_t *self, const char *format, ...);
+
+int zyre_start (zyre_t *self);
+
+void zyre_stop (zyre_t *self);
+
+int zyre_join (zyre_t *self, const char *group);
+
+int zyre_leave (zyre_t *self, const char *group);
+
+zmsg_t* zyre_recv (zyre_t *self);
+
+int zyre_whisper (zyre_t *self, const char *peer, zmsg_t **msg_p);
+
+int zyre_shout (zyre_t *self, const char *group, zmsg_t **msg_p);
+
+int zyre_whispers (zyre_t *self, const char *peer, const char *format, ...);
+
+int zyre_shouts (zyre_t *self, const char *group, const char *format, ...);
+
+zsock_t* zyre_socket (zyre_t *self);
+
+void zyre_dump (zyre_t *self);
+
+void zyre_version (int *major, int *minor, int *patch);
+
+void zyre_test (bool verbose);
+
+typedef struct _zyre_event_t zyre_event_t;
+typedef struct _zhash_t zhash_t;
+
+typedef enum {
+    ZYRE_EVENT_ENTER = 1,
+    ZYRE_EVENT_JOIN = 2,
+    ZYRE_EVENT_LEAVE = 3,
+    ZYRE_EVENT_EXIT = 4,
+    ZYRE_EVENT_WHISPER = 5,
+    ZYRE_EVENT_SHOUT = 6
+} zyre_event_type_t;
+
+zyre_event_t* zyre_event_new (zyre_t *self);
+
+void zyre_event_destroy (zyre_event_t **self_p);
+
+zyre_event_type_t zyre_event_type (zyre_event_t *self);
+
+char *
+    zyre_event_sender (zyre_event_t *self);
+
+//  Return the sending peer's public name as a string
+ char *
+    zyre_event_name (zyre_event_t *self);
+
+//  Return the sending peer's ipaddress as a string
+ char *
+    zyre_event_address (zyre_event_t *self);
+
+//  Returns value of a header from the message headers
+//  obtained by ENTER. Return NULL if no value was found.
+ char *
+    zyre_event_header (zyre_event_t *self, char *name);
+
+//  Returns the group name that a SHOUT event was sent to
+ char *
+    zyre_event_group (zyre_event_t *self);
+
+//  Returns the incoming message payload (currently one frame)
+ zmsg_t *
+    zyre_event_msg (zyre_event_t *self);
+
+zhash_t *
+    zyre_event_headers (zyre_event_t *self);
+
+const char * zsys_interface ();
+''')
+
+# os.environ['PATH'] = os.environ['PATH'] + os.path.abspath(os.path.dirname(__file__))
+
+zyre_lib = ffi.dlopen( os.path.abspath(os.path.join(os.path.dirname(__file__), 'zyre.dll')))
+czmq_lib = ffi.dlopen( os.path.abspath(os.path.join(os.path.dirname(__file__), 'czmq.dll')))
+
+new_int = lambda: ffi.new('int*')
---
+++ zyrecffi/zyrecffi.py
@@ -0,0 +1,105 @@
+from _cffi import *
+
+def zyre_version():
+    major, minor, patch  = ffi.new('int*'),  ffi.new('int*'),  ffi.new('int*')
+    zyre_lib.zyre_version(major, minor, patch)
+    return (major[0], minor[0], patch[0])
+
+class ZyreException(Exception):
+    def __init__(self, error):
+        self._error = error
+
+    def __repr__(self):
+        return 'ZyreException("{}")'.format(self._error)
+
+    def __str__(self):
+        return 'ZyreError: {}'.format(self._error)
+
+class ZyreEvent(object):
+    def __init__(self, zyre_event_t):
+        self._z_event = zyre_event_t
+
+    def __del__(self):
+        zyre_lib.zyre_event_destroy(ffi.new('zyre_event_t**', self._z_event))
+
+    def sender(self):
+        return zyre_lib.zyre_event_sender(self._z_event)
+
+    def name(self):
+        return zyre_lib.zyre_event_name(self._z_event)
+
+    def address(self):
+        return zyre_lib.zyre_event_address(self._z_event)
+
+    def group(self):
+        return zyre_lib.zyre_event_group(self._z_event)
+
+class ZyreNode(object):
+    def __init__(self, name = '', verbose=False):
+        self._z_node = zyre_lib.zyre_new(name)
+        if (verbose):
+            zyre_lib.zyre_set_verbose(self._z_node)
+        self.groups = set()
+
+        self._port = 5670 # default port
+        self._interface = ''
+
+    def __del__(self):
+        self.stop()
+        zyre_lib.zyre_destroy(ffi.new('zyre_t**',self._z_node))
+
+    @property
+    def name(self):
+        return ffi.string(zyre_lib.zyre_name(self._z_node))
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, value):
+        self._port = value
+        zyre_lib.zyre_set_port(self._z_node, value)
+
+    @property
+    def uuid(self):
+        return ffi.string(zyre_lib.zyre_uuid(self._z_node))
+
+    @property
+    def interface(self):
+        return ffi.string(czmq_lib.zsys_interface())
+
+    @interface.setter
+    def interface(self, value):
+        zyre_lib.zyre_set_interface(self._z_node, value)
+
+    def set_header(self, name, value):
+        zyre_lib.zyre_set_header(self._z_node, name, value)
+
+    def set_interval(self, value):
+        zyre_lib.zyre_set_interval(self._z_node, value)
+
+    def start(self):
+        if zyre_lib.zyre_start(self._z_node) == 1:
+            raise ZyreNode('Failed to start beacon')
+
+    def stop(self):
+        zyre_lib.zyre_stop(self._z_node)
+
+    def join(self, group):
+        zyre_lib.zyre_join(self._z_node, group)
+        self.groups.add(group)
+
+    def leave(self, group):
+        zyre_lib.zyre_leave(self._z_node, group)
+        self.groups.remove(group)
+
+    def whispers(self, peer, msg_string):
+        zyre_lib.zyre_whispers(self._z_node, peer, msg_string)
+
+    def shouts(self, group, msg_string):
+        zyre_lib.zyre_shouts(self._z_node, group, msg_string)
+
+    def dump(self):
+        zyre_lib.zyre_dump(self._z_node)
+
